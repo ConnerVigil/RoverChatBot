17195d79614c34189a0c2e28d73c20a70851bc36
Conner Vigil <cjvigil@live.com>
2023-12-23 16:53:13 -0700

    add get current time function

--- chatbot.py
+++ chatbot.py
@@ -243,6 +243,124 @@ start_chat_log = [
 ]
 
 
+def askgpt(question: str, chat_log: list = None) -> tuple[str, list]:
+    # If there is no chat log, start with the context
+    if chat_log is None:
+        chat_log = start_chat_log
+
+    chat_log = chat_log + [{"role": "user", "content": question}]
+
+    response = openAI_client.chat.completions.create(
+        model="gpt-3.5-turbo-1106",
+        messages=chat_log,
+        tools=tools,
+        tool_choice="auto",
+    )
+
+    response_message = response.choices[0].message
+    tool_calls = response_message.tool_calls
+
+    # Check if the model wanted to call a function
+    if tool_calls:
+        available_functions = {
+            "book_appointment": {
+                "function": book_appointment,
+                "parameters": ["date", "time"],
+            },
+            "end_conversation": {
+                "function": end_conversation,
+                "parameters": [],
+            },
+            "get_current_date_and_time": {
+                "function": get_current_date_and_time,
+                "parameters": [],
+            },
+        }
+
+        # Iterate through tool_calls and restructure into json to send back to the model
+        new_tool_calls = []
+        for tool_call in tool_calls:
+            temp = {
+                "id": tool_call.id,
+                "type": "function",
+                "function": {
+                    "name": tool_call.function.name,
+                    "arguments": tool_call.function.arguments,
+                },
+            }
+            new_tool_calls.append(temp)
+
+        newMessage = {
+            "role": "assistant",
+            "content": None,
+            "tool_calls": new_tool_calls,
+        }
+
+        # extend conversation with model's reply
+        chat_log.append(newMessage)
+
+        for tool_call in tool_calls:
+            function_name = tool_call.function.name
+            function_info = available_functions.get(function_name)
+
+            if function_info:
+                function_to_call = function_info["function"]
+                function_parameters = function_info["parameters"]
+
+                function_args = json.loads(tool_call.function.arguments)
+                function_args = {
+                    param: function_args.get(param) for param in function_parameters
+                }
+
+                function_response = function_to_call(**function_args)
+                chat_log.append(
+                    {
+                        "tool_call_id": tool_call.id,
+                        "role": "tool",
+                        "name": function_name,
+                        "content": function_response,
+                    }
+                )
+
+        # get a new response from the model where it can see the function response
+        second_response = openAI_client.chat.completions.create(
+            model="gpt-3.5-turbo-1106", messages=chat_log
+        )
+
+        answer = second_response.choices[0].message.content
+    else:
+        answer = response.choices[0].message.content
+
+    chat_log = chat_log + [{"role": "assistant", "content": answer}]
+    pretty_print_conversation(chat_log)
+    return answer, chat_log
+
+
+async def send_message(content: str, sender_number: str, twilio_number: str) -> None:
+    message = twilio_client.messages.create(
+        body=content,
+        from_=twilio_number,
+        to=sender_number,
+    )
+    return
+
+
+def book_appointment(date: str, time: str) -> str:
+    print("Booking Appointment...")
+    return json.dumps({"date booked": date, "time booked": time})
+
+
+def end_conversation():
+    session.clear()
+    print("Conversation Ended")
+    return json.dumps({"end conversation": True})
+
+
+def get_current_date_and_time() -> str:
+    current_date_time = datetime.now()
+    return current_date_time.isoformat()
+
+
 def pretty_print_conversation(messages):
     role_to_color = {
         "system": "red",
@@ -283,111 +401,3 @@ def pretty_print_conversation(messages):
                     role_to_color[message["role"]],
                 )
             )
-
-
-def askgpt(question: str, chat_log: list = None) -> tuple[str, list]:
-    # If there is no chat log, start with the context
-    if chat_log is None:
-        chat_log = start_chat_log
-
-    chat_log = chat_log + [{"role": "user", "content": question}]
-
-    response = openAI_client.chat.completions.create(
-        model="gpt-3.5-turbo-1106",
-        messages=chat_log,
-        tools=tools,
-        tool_choice="auto",
-    )
-
-    response_message = response.choices[0].message
-    # print(response_message)
-    tool_calls = response_message.tool_calls
-
-    # Check if the model wanted to call a function
-    if tool_calls:
-        
-        available_functions = {
-            "book_appointment": book_appointment,
-        }
-        
-        # Iterate through tool_calls and restructure into json to send back to the model
-        new_tool_calls = []
-        for tool_call in tool_calls:
-            temp = {
-                "id": tool_call.id,
-                "type": "function",
-                "function": {
-                    "name": tool_call.function.name,
-                    "arguments": tool_call.function.arguments,
-                }
-            }
-            new_tool_calls.append(temp)
-
-        newMessage = {
-            "role": "assistant",
-            "content": None,
-            "tool_calls": new_tool_calls,
-        }
-
-        # extend conversation with model's reply
-        chat_log.append(newMessage)
-
-        # send the info for each function call and function response to the model
-        for tool_call in tool_calls:
-            function_name = tool_call.function.name
-            function_to_call = available_functions[function_name]
-            function_args = json.loads(tool_call.function.arguments)
-
-            function_response = function_to_call(
-                date=function_args.get("date"),
-                time=function_args.get("time"),
-            )
-
-            # extend conversation with function response
-            chat_log.append(
-                {
-                    "tool_call_id": tool_call.id,
-                    "role": "tool",
-                    "name": function_name,
-                    "content": function_response,
-                }
-            )
-
-        # get a new response from the model where it can see the function response
-        second_response = openAI_client.chat.completions.create(
-            model="gpt-3.5-turbo-1106", messages=chat_log
-        )
-
-        answer = second_response.choices[0].message.content
-    else:
-        answer = response.choices[0].message.content
-
-    chat_log = chat_log + [{"role": "assistant", "content": answer}]
-    pretty_print_conversation(chat_log)
-    return answer, chat_log
-
-
-async def send_message(content: str, sender_number: str, twilio_number: str) -> None:
-    message = twilio_client.messages.create(
-        body=content,
-        from_=twilio_number,
-        to=sender_number,
-    )
-    return
-
-
-def book_appointment(date: str, time: str) -> str:
-    print("Booking Appointment...")
-    return json.dumps({"date booked": date, "time booked": time})
-
-
-def end_conversation():
-    session.clear()
-    return json.dumps({"end conversation": True})
-
-
-def get_current_date_and_time():
-    current_date_time = datetime.now()
-    print(current_date_time)
-    return current_date_time
-
