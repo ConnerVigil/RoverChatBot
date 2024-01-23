94c66f91cd640a5955269ffc374e0a65dfc5aa2a
Thomas Chappell <jthomaschappell@gmail.com>
2024-01-23 11:03:37 -0700

    Refactor - move code that uses openAI client to openai_services.py

--- openai_services.py
+++ openai_services.py
@@ -1,1 +1,15 @@
 from clients import openai_client
+
+def create_gpt_response(model, messages, tools=None, tool_choice=None):
+    "takes named arguments and returns the response that GPT creates"
+    if tools is None and tool_choice is None:
+        return openai_client.chat.completions.create(
+            model=model,
+            messages=messages
+        )
+    return openai_client.chat.completions.create(
+        model=model,
+        messages=messages,
+        tools=tools,
+        tool_choice=tool_choice,
+    ) 
