ed9badbec5130079f566cddd023d98556d4cc5d3
Thomas Chappell <jthomaschappell@gmail.com>
2024-02-02 16:01:00 -0700

    i hate supabse

---
+++ supabase/migrations/20240202230012_add_missed_calls.sql
@@ -0,0 +1,60 @@
+create table "public"."Missed_Calls" (
+    "id" uuid not null default gen_random_uuid(),
+    "created_at" timestamp with time zone not null default now(),
+    "conversation_id" uuid not null,
+    "to_phone_number" text not null,
+    "from_phone_number" text not null
+);
+
+
+CREATE UNIQUE INDEX "Missed_Calls_pkey" ON public."Missed_Calls" USING btree (id);
+
+alter table "public"."Missed_Calls" add constraint "Missed_Calls_pkey" PRIMARY KEY using index "Missed_Calls_pkey";
+
+alter table "public"."Missed_Calls" add constraint "Missed_Calls_conversation_id_fkey" FOREIGN KEY (conversation_id) REFERENCES "Conversations"(id) not valid;
+
+alter table "public"."Missed_Calls" validate constraint "Missed_Calls_conversation_id_fkey";
+
+grant delete on table "public"."Missed_Calls" to "anon";
+
+grant insert on table "public"."Missed_Calls" to "anon";
+
+grant references on table "public"."Missed_Calls" to "anon";
+
+grant select on table "public"."Missed_Calls" to "anon";
+
+grant trigger on table "public"."Missed_Calls" to "anon";
+
+grant truncate on table "public"."Missed_Calls" to "anon";
+
+grant update on table "public"."Missed_Calls" to "anon";
+
+grant delete on table "public"."Missed_Calls" to "authenticated";
+
+grant insert on table "public"."Missed_Calls" to "authenticated";
+
+grant references on table "public"."Missed_Calls" to "authenticated";
+
+grant select on table "public"."Missed_Calls" to "authenticated";
+
+grant trigger on table "public"."Missed_Calls" to "authenticated";
+
+grant truncate on table "public"."Missed_Calls" to "authenticated";
+
+grant update on table "public"."Missed_Calls" to "authenticated";
+
+grant delete on table "public"."Missed_Calls" to "service_role";
+
+grant insert on table "public"."Missed_Calls" to "service_role";
+
+grant references on table "public"."Missed_Calls" to "service_role";
+
+grant select on table "public"."Missed_Calls" to "service_role";
+
+grant trigger on table "public"."Missed_Calls" to "service_role";
+
+grant truncate on table "public"."Missed_Calls" to "service_role";
+
+grant update on table "public"."Missed_Calls" to "service_role";
+
+
