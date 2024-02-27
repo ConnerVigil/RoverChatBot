5acf4997129a8cff8c3786b4bee34f7cf4dcdc7b
Conner Vigil <cjvigil@live.com>
2024-02-27 14:09:49 -0700

    bugs

---
+++ supabase/migrations/20240227204953_AddVoicemailInfoToMissedCalls.sql
@@ -0,0 +1,9 @@
+alter table "public"."Missed_Calls" add column "call_sid" text;
+
+alter table "public"."Missed_Calls" add column "recording_sid" text;
+
+alter table "public"."Missed_Calls" add column "recording_transcription" text;
+
+alter table "public"."Missed_Calls" add column "recording_url" text;
+
+
